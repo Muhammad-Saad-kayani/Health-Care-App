6c9efbef8a1eac5e1a4a7680713bc4a54c61c4de
MusadiqueMuslim1997 <musadique1997@gmail.com>
2024-08-31 20:34:21 +0500

    add footer

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,12 @@
 import Image from "next/image";
 import Navbar from "./Components/Navbar";
+import Footer from "./Components/Footer";
 
 export default function Home() {
   return (
+    <>
     <Navbar/>
+    <Footer/>
+    </>
   );
 }
