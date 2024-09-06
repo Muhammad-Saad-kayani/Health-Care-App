d0789f09d58457104a7aec533c42e9f0c14dab15
MusadiqueMuslim1997 <musadique1997@gmail.com>
2024-09-06 20:44:06 +0500

    add

--- src/app/Components/medicineCard.jsx
+++ src/app/Components/medicineCard.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faAngleRight } from "@fortawesome/free-solid-svg-icons";
 
 function MedicineCard() {
   return (
@@ -13,7 +15,7 @@ function MedicineCard() {
           </div>
 
           <div className="w-full flex justify-between mt-5 gap-5">
-            <div className="w-[48%]  bg-[#edf3fc] hover:bg-blehover cursor-pointer flex items-center justify-center flex-wrap p-5">
+            <div className="w-[49%]  bg-[#edf3fc] hover:bg-blehover cursor-pointer flex items-center justify-center flex-wrap p-5">
               <div className="w-full flex justify-center ">
                 <img src="./med1.webp" alt="" width={350} />
               </div>
@@ -28,16 +30,18 @@ function MedicineCard() {
                 </div>
               </div>
             </div>
-            <div className="w-[48%] cursor-pointer flex items-center justify-between flex-wrap gap-4 ">
+            <div className="w-[49%] cursor-pointer flex items-center justify-between flex-wrap gap-4 ">
               <div className="w-[48%] h-72 rounded-xl bg-[#edf3fc] hover:bg-blehover p-5 flex items-center  flex-wrap justify-center gap-1">
                 <div className="w-full flex items-center justify-center">
                   <img src="./med2.webp" alt="" width={150} />
                 </div>
                 <div className="w-full flex flex-wrap gap-2">
-                <div className="w-full text-lg">
-                  Risek Insta (200mg 10 Sachets)
-                </div>
-                <div className="w-full text-sm text-gray-500">Azithromycin</div>
+                  <div className="w-full text-lg">
+                    Risek Insta (200mg 10 Sachets)
+                  </div>
+                  <div className="w-full text-sm text-gray-500">
+                    Azithromycin
+                  </div>
                 </div>
                 <div className="w-full flex items-center gap-5">
                   <span>Rs. 19.91 </span>{" "}
@@ -51,10 +55,12 @@ function MedicineCard() {
                   <img src="./med3.webp" alt="" width={"90%"} />
                 </div>
                 <div className="w-full flex flex-wrap gap-2">
-                <div className="w-full text-lg">Concor (2.5mg) 14 Tablets</div>
-                <div className="w-full text-sm text-gray-500">
-                  Merck Marker (Pvt) Ltd.
-                </div>
+                  <div className="w-full text-lg">
+                    Concor (2.5mg) 14 Tablets
+                  </div>
+                  <div className="w-full text-sm text-gray-500">
+                    Merck Marker (Pvt) Ltd.
+                  </div>
                 </div>
                 <div className="w-full flex items-center gap-5">
                   <span>Rs. 157.5 </span>{" "}
@@ -68,12 +74,12 @@ function MedicineCard() {
                   <img src="./med4.png" alt="" width={50} />
                 </div>
                 <div className="w-full flex flex-wrap gap-2">
-                <div className="w-full text-lg">
-                  Brufen (100mg/5ml) 120ml Oral Suspension
-                </div>
-                <div className="w-full text-sm text-gray-500">
-                  Abbot Laboratories (Pak) Ltd
-                </div>
+                  <div className="w-full text-lg">
+                    Brufen (100mg/5ml) 120ml Oral Suspension
+                  </div>
+                  <div className="w-full text-sm text-gray-500">
+                    Abbot Laboratories (Pak) Ltd
+                  </div>
                 </div>
                 <div className="w-full flex items-center gap-5">
                   <span>Rs. 105.3 </span>{" "}
@@ -87,21 +93,31 @@ function MedicineCard() {
                   <img src="./med5.webp" alt="" width={50} />
                 </div>
                 <div className="w-full flex flex-wrap gap-2">
-                <div className="w-full text-lg">
-                  GAVISCON LIQUID 120ML   
-                </div>
-                <div className="w-full text-sm text-gray-500">Reckitt Benckiser Pak Ltd.</div>
+                  <div className="w-full text-lg">GAVISCON LIQUID 120ML</div>
+                  <div className="w-full text-sm text-gray-500">
+                    Reckitt Benckiser Pak Ltd.
+                  </div>
                 </div>
                 <div className="w-full flex items-center gap-5">
                   <span>Rs. 179.1</span>{" "}
                   <span className="text-sm text-gray-500">
-                    <s>Rs.
-                    199.0</s>
+                    <s>Rs. 199.0</s>
                   </span>
                 </div>
               </div>
             </div>
           </div>
+          <a
+            href="/"
+            className="w-full text-right text-[#3d6093] flex gap-2 justify-end  text-sm font-sans font-bold"
+          >
+            <div className="">
+              View Online Pharmacy{" "}
+              <span className="font-sm font-bold">
+                <FontAwesomeIcon icon={faAngleRight} className="size-3" />
+              </span>
+            </div>
+          </a>
         </div>
       </section>
     </>
