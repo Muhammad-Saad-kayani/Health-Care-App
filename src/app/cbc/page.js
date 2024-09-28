70d6f324084fba11ee541b39eceee734365cfea9
MusadiqueMuslim1997 <musadique1997@gmail.com>
2024-09-28 22:31:08 +0500

    add Cbc page

--- src/app/cbc/page.js
+++ src/app/cbc/page.js
@@ -9,35 +9,183 @@ function Cbc() {
 
       <section className="containers mt-40 bg-white">
         <div className="navsec w-full flex  justify-between flex-wrap gap-2">
-         <div className="w-full flex justify-between">
-         <div className="w-[60%] flex  flex-wrap gap-5">
-         <div className="w-full text-3xl text-[#183155] font-sans font-bold">CBC BLOOD TEST</div>
-            </div>   
-         <div className="w-[35%] flex justify-end">
-            <div className="w-[380px] border-1 rounded-lg bg-[#edf3fc] flex p-6 flex-wrap">
-                <div className="w-full font-sans text-md font-bold text-[#1e4275]">Book this lab test?</div>
-                <div className="w-full flex justify-between mt-4">
-                    <div className="w-[50%] font-sans text-sm">CBC blood test</div>
-                    <div className="w-[50%] text-end text-sm "><span className="font-bold ">Rs. 680.0</span> <s>Rs.800</s></div>
+          <div className="w-full flex justify-between">
+            <div className="w-[50%] flex  flex-col">
+              <div className="w-full text-3xl text-[#183155] font-sans font-bold">
+                CBC BLOOD TEST
+              </div>
+              <div className="w-full font-sans font-medium mt-8">
+                What is the Complete Blood Count (CBC) Test?
+              </div>
+              <div className="w-full font-sans text-[0.8rem] text-gray-600 mt-8">
+                Also known as CBC, complete blood count test, CBC complete blood
+                count test, blood CP, blood routine examination, blood C/E
+                (Complete, CBC), blood complete profile or blood complete
+                picture, it is a group of tests that provide information about
+                the blood cells such as Red Blood Cells (RBC), White Blood Cells
+                (WBC) and Platelets.
+              </div>
+              <div className="w-full font-sans text-[0.8rem] text-gray-600 mt-8">
+                CBC test in Pakistan is a simple routine test that helps medical
+                practitioners to get to know the overall health of an
+                individual.
+              </div>
+              <div className="w-full font-sans text-[0.8rem] text-gray-600 mt-8">
+                Medical practitioners at Top Hospitals i.e.{" "}
+                <span className="text-[#1e4275] font-sans font-bold">
+                  Doctors Hospital{" "}
+                </span>
+                recommend a CBC test if you have the following symptoms:
+              </div>
+              <div className="w-full font-sans text-[0.8rem] text-gray-600 mt-8">
+                <ul className="w-full list-disc pl-5">
+                  <li>Fever, vomiting or nausea</li>
+                  <li>Bruising and bleeding</li>
+                  <li>Weakness, fatigue or weakness</li>
+                  <li>Problem with blood pressure or heart rate</li>
+                  <li>
+                    Inflammation (Irritation and swelling) anywhere in the body
+                  </li>
+                  <li>Joint pain</li>
+                </ul>
+              </div>
+              <div className="w-full font-sans font-medium mt-8">
+                CBC Test Price in Pakistan
+              </div>
+              <div className="w-full font-sans text-[0.8rem] text-gray-600 mt-8">
+                CBC test price in Pakistan ranges between (500-1000) Rupees. The
+                CBC test cost in Pakistan varies depending upon the lab and city
+                you are choosing to get your test. However, to get a discount on
+                CBC test prices in Karachi, Lahore, Islamabad, Rawalpindi,
+                Multan, and other major cities, you can book your CBC test from
+                Healthwire.
+              </div>
+              <div className="w-full font-sans font-medium mt-8">
+                How Can I Book CBC Test Near Me?
+              </div>
+              <div className="w-full font-sans text-[0.8rem] text-gray-600 mt-8">
+                You can easily book a CBC test online. Doctors at{" "}
+                <span className="text-[#1e4275] font-sans font-bold">
+                  Hameed Latif Hospital{" "}
+                </span>{" "}
+                rely on Healthwire and refer patients there for efficient
+                booking of CBC Test. To book a CBC test at affordable price in
+                Lahore, Karachi, Islamabad, Rawalpindi, and other major cities,
+                you can call us at 042352500989/02137130261 or click on the test
+                for instant booking.
+              </div>
+              <div className="w-full font-sans text-[0.8rem] text-gray-600 mt-8">
+                To book your CBC test:
+              </div>
+              <div className="w-full font-sans text-[0.8rem] text-gray-600 mt-8">
+                <ul className="w-full list-disc pl-5">
+                  <li>
+                    You can simply click on the lab test online via our web
+                    healthwire.pk or Healthwire app{" "}
+                  </li>
+                  <li>Select CBC test or upload prescription</li>
+                  <li>Pay in cash or online (debit/credit card, jazzcash)</li>
+                  <li>You can also opt for free home sampling </li>
+                  <li>Our representative will call you for confirmation</li>
+                </ul>
+              </div>
+              <div className="w-full font-sans text-[0.8rem] text-gray-600 mt-8">
+                You can also call us at 042352500989 or click on the test for
+                instant booking. You can also avail discount on the CBC blood
+                test price if you book your test via healthwire.
+              </div>
+              <div className="w-full font-sans font-medium mt-8">
+                Why is a Complete Blood Count (CBC) Test Conducted?
+              </div>
+              <div className="w-full font-sans text-[0.8rem] text-gray-600 mt-8">
+                This test is conducted to:
+              </div>
+              <div className="w-full font-sans text-[0.8rem] text-gray-600 mt-8">
+                <ul className="w-full list-disc pl-5">
+                  <li>Evaluate the overall health of a person</li>
+                  <li>Rule out diseases and disorders</li>
+                  <li>Detect any abnormality in the blood</li>
+                  <li>Detect any abnormality in the blood </li>
+                  <li>
+                    Access and diagnose different blood infections, conditions,
+                    diseases and disorders
+                  </li>
+                  <li>Monitor different blood diseases and disorders</li>
+                  <li>
+                    Monitor the efficacy of the treatment for certain
+                    conditions, diseases and disorders such as during
+                    radiotherapy and chemotherapy{" "}
+                  </li>
+                </ul>
+              </div>
+              <div className="w-full font-sans text-[0.8rem] text-gray-600 mt-8">
+                It is a routine test that is conducted on a daily basis in the
+                labs of Lahore, Karachi, Multan, Rawalpindi, Islamabad, and
+                other major cities.
+              </div>
+              <div className="w-full font-sans font-medium mt-8">
+                What is a CBC Test Good For?
+              </div>
+              <div className="w-full font-sans text-[0.8rem] text-gray-600 mt-8">
+                The CBC test done in Lahore, Karachi, Islamabad, Multan,
+                Rawalpindi and all over Pakistan helps your medical practitioner
+                in diagnosing and assessing various kinds of infections,
+                diseases and disorders such as:
+              </div>
+              <div className="w-full font-sans text-[0.8rem] text-gray-600 mt-8">
+                <ul className="w-full list-disc pl-5">
+                  <li>Anemia</li>
+                  <li>
+                    Disorders such as sickle cell anemia, thalassemia and
+                    agranulocytosis
+                  </li>
+                  <li>
+                    Infections that result in low WBC levels or high WBC levels{" "}
+                  </li>
+                  <li>Minerals and vitamin deficiencies </li>
+                  <li>
+                    Bone marrow disorders such as myelodysplastic syndromes
+                  </li>
+                  <li>Blood cancers (<span className="text-[#1e4275] font-sans font-bold">leukemia </span> and <span className="text-[#1e4275] font-sans font-bold">lymphoma </span>)</li>
+                  <li>Side effects of any medication or chemotherapy</li>
+                </ul>
+              </div>
+            </div>
 
+            <div className="w-[45%] flex justify-end">
+              <div className="w-[380px] h-60 border-1 rounded-lg bg-[#edf3fc] flex p-6 flex-wrap">
+                <div className="w-full font-sans text-md font-bold text-[#1e4275]">
+                  Book this lab test?
+                </div>
+                <div className="w-full flex justify-between mt-4">
+                  <div className="w-[50%] font-sans text-sm">
+                    CBC blood test
+                  </div>
+                  <div className="w-[50%] text-end text-sm ">
+                    <span className="font-bold ">Rs. 680.0</span> <s>Rs.800</s>
+                  </div>
                 </div>
                 <div className="w-full flex justify-end mt-4">
-                    <button className="text-sm border-2 p-2 h-8 flex items-center text-[#1e4275] rounded-sm border-[#1e4275]">Add to cart</button>
+                  <button className="text-sm border-2 p-2 h-8 flex items-center text-[#1e4275] rounded-sm border-[#1e4275]">
+                    Add to cart
+                  </button>
                 </div>
                 <div className="w-full flex justify-between mt-10">
-                    <div className="w-[49%]">
-                    <button className="w-full text-sm border-2 p-2 h-10 flex items-center justify-center  text-[#1e4275] rounded-sm border-[#1e4275]">Back to Tests</button>
-                    </div>
-                    <div className="w-[49%] flex justify-end">
-                    <button className="w-full text-sm border-2 p-2 h-10 flex items-center justify-center text-white rounded-sm bg-[#1e4275] border-[#1e4275]">Checkout</button>
-                    </div>
-
+                  <div className="w-[49%]">
+                    <button className="w-full text-sm border-2 p-2 h-10 flex items-center justify-center  text-[#1e4275] font-semibold rounded-sm border-[#1e4275]">
+                      Back to Tests
+                    </button>
+                  </div>
+                  <div className="w-[49%] flex justify-end">
+                    <button className="w-full text-sm border-2 p-2 h-10 flex items-center justify-center text-white font-semibold rounded-sm bg-[#1e4275] border-[#1e4275]">
+                      Checkout
+                    </button>
+                  </div>
                 </div>
+              </div>
             </div>
-         </div>
-         </div>
+          </div>
         </div>
-     
       </section>
 
       {/* <Footer /> */}
